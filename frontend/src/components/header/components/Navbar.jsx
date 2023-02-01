ade5df2c09516baa0e91b4845f8c5934f6cca336
GuiPich <110974962+GuiPich@users.noreply.github.com>
2023-02-01 15:01:58 +0100

    structure de base du site terminée

--- frontend/src/components/header/components/Navbar.jsx
+++ frontend/src/components/header/components/Navbar.jsx
@@ -9,14 +9,14 @@ const Navbar = () => {
         <div className='navbar'>
             <div className="navbarBody">
                 <div className="navbarBodyRight">
-                    <img id="photoProfil" src={Gui} alt="Avatar de Guillaume Pichaud" style={{ width: 50 }} />
+                    <NavLink to="/"><img id="photoProfil" src={Gui} alt="Avatar de Guillaume Pichaud" style={{ width: 50 }} /></NavLink>
                 </div>
                 <div className="navbarBodyLeft">
                     <NavLink to="/">About</NavLink>
                     <NavLink to="/portfolio">Portfolio</NavLink>
                     <NavLink to="/contact">Contact</NavLink>
-                    <NavLink to=""><AiOutlineLinkedin className="navbarIcon" /></NavLink>
-                    <NavLink to=""><FiGithub className="navbarIcon" /></NavLink>
+                    <NavLink to="https://www.linkedin.com/in/guillaume-pichaud-627b308a/"><AiOutlineLinkedin className="navbarIcon" /></NavLink>
+                    <NavLink to="https://github.com/GuiPich"><FiGithub className="navbarIcon" /></NavLink>
                 </div>
             </div>
         </div>
--- frontend/src/components/profil/Profil.jsx
+++ frontend/src/components/profil/Profil.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import Gui from '@assets/Navbar/Gui.png';
 
 const Profil = () => {
     return (
         <div className='profil'>
-            <h3>Guillaume Pichaud</h3>
-
+            <div className="profilCard">
+                <div className="cardRight">
+                    <h3>Hi ! I'M GUILLAUME PICHAUD</h3>
+                    <p>A fullstack Web Developer building the Frontend and the Backend of Websites and Web Apllications that leads to the succes of your product</p>
+                </div>
+                <div className="cardLeft">
+                    <img src={Gui} alt="illustration de Guillaume" className="photoLandingPage" />
+                </div>
+            </div>
         </div>
     );
 };
